bf9ea5540c4165d0164a3799d31e3e293a5ea5df
iutin <evgeniy.iutin@gmail.com>
2016-03-31 11:10:31 +0300

    install plugin

--- wp-content/themes/shaker-theme/inc/cpt.php
+++ wp-content/themes/shaker-theme/inc/cpt.php
@@ -5,13 +5,13 @@ add_action('init', 'custom_post_type', 0);
 function custom_post_type()
 {
     $labels = array(
-        'name' => 'Clients',
-        'singular_name' => 'Clients',
-        'menu_name' => 'Clients',
-        'all_items' => 'All clients',
-        'view_item' => 'View client',
-        'add_new_item' => 'Add client',
-        'add_new' => 'Add client',
+        'name' => 'Images',
+        'singular_name' => 'Images',
+        'menu_name' => 'Images',
+        'all_items' => 'All images',
+        'view_item' => 'View image',
+        'add_new_item' => 'Add image',
+        'add_new' => 'Add image',
         'edit_item' => 'Edit',
         'update_item' => 'Update',
         'search_items' => 'Search'
@@ -36,178 +36,6 @@ function custom_post_type()
         )
     );
 
-    register_post_type('clients', $args);
+    register_post_type('images', $args);
 
-    $labels = array(
-        'name' => 'Team',
-        'singular_name' => 'Team',
-        'menu_name' => 'Team',
-        'all_items' => 'All team',
-        'view_item' => 'View person',
-        'add_new_item' => 'Add author',
-        'add_new' => 'Add person',
-        'edit_item' => 'Edit',
-        'update_item' => 'Update',
-        'search_items' => 'Search'
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'supports' => array('title','editor'),
-        'hierarchical' => false,
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'show_in_admin_bar' => true,
-        'can_export' => true,
-        'has_archive' => false,
-        'menu_position'=> 24,
-        'exclude_from_search' => true,
-        'publicly_queryable' => true,
-        'capability_type' => 'post',
-        'rewrite' => array(
-            'with_front' => true
-        )
-    );
-
-    register_post_type('team', $args);
-
-    $labels = array(
-        'name' => 'Service',
-        'singular_name' => 'Service',
-        'menu_name' => 'Services',
-        'all_items' => 'All services',
-        'view_item' => 'View service',
-        'add_new_item' => 'Add service',
-        'add_new' => 'Add service',
-        'edit_item' => 'Edit',
-        'update_item' => 'Update',
-        'search_items' => 'Search'
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'supports' => array('title','editor','excerpt'),
-        'hierarchical' => false,
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'show_in_admin_bar' => true,
-        'can_export' => true,
-        'has_archive' => false,
-        'menu_position'=> 28,
-        'exclude_from_search' => true,
-        'publicly_queryable' => true,
-        'capability_type' => 'post',
-        'rewrite' => array(
-            'with_front' => true
-        )
-    );
-
-    register_post_type('services', $args);
-    $labels = array(
-        'name' => 'Case studies',
-        'singular_name' => 'Case study',
-        'menu_name' => 'Case studies',
-        'all_items' => 'All Case studies',
-        'view_item' => 'View Case study',
-        'add_new_item' => 'Add Case study',
-        'add_new' => 'Add Case study',
-        'edit_item' => 'Edit',
-        'update_item' => 'Update',
-        'search_items' => 'Search'
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'supports' => array('title','editor','excerpt'),
-        'hierarchical' => false,
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'show_in_admin_bar' => true,
-        'can_export' => true,
-        'has_archive' => false,
-        'menu_position'=> 29,
-        'exclude_from_search' => true,
-        'publicly_queryable' => true,
-        'capability_type' => 'post',
-        'rewrite' => array(
-            'with_front' => true
-        )
-    );
-
-    register_post_type('case_studies', $args);
-}
-
-// хук для регистрации
-add_action('init', 'create_taxonomy');
-function create_taxonomy(){
-    // заголовки
-    $labels = array(
-        'name'              => 'Team Categories',
-        'singular_name'     => 'Team Category',
-        'search_items'      => 'Search Category',
-        'all_items'         => 'All Categories',
-        'parent_item'       => 'Parent Category',
-        'parent_item_colon' => 'Parent Category:',
-        'edit_item'         => 'Edit Category',
-        'update_item'       => 'Update Category',
-        'add_new_item'      => 'Add New Category',
-        'new_item_name'     => 'New Category Name',
-        'menu_name'         => 'Categories',
-    );
-    // параметры
-    $args = array(
-        'label'                 => '', // определяется параметром $labels->name
-        'labels'                => $labels,
-        'public'                => true,
-        'show_in_nav_menus'     => true, // равен аргументу public
-        'show_ui'               => true, // равен аргументу public
-        'show_tagcloud'         => true, // равен аргументу show_ui
-        'hierarchical'          => false,
-        'update_count_callback' => '',
-        'rewrite'               => true,
-        //'query_var'             => $taxonomy, // название параметра запроса
-        'capabilities'          => array(),
-        'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
-        'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
-        '_builtin'              => false,
-        'show_in_quick_edit'    => null, // по умолчанию значение show_ui
-    );
-    register_taxonomy('p_categories', array('team'), $args );
-
-    // заголовки
-    $labels = array(
-        'name'              => 'Service Types',
-        'singular_name'     => 'Service Type',
-        'search_items'      => 'Search Service',
-        'all_items'         => 'All Services',
-        'parent_item'       => 'Parent Service',
-        'parent_item_colon' => 'Parent Service:',
-        'edit_item'         => 'Edit Service',
-        'update_item'       => 'Update Service',
-        'add_new_item'      => 'Add New Service',
-        'new_item_name'     => 'New Service Name',
-        'menu_name'         => 'Service Types',
-    );
-    // параметры
-    $args = array(
-        'label'                 => '', // определяется параметром $labels->name
-        'labels'                => $labels,
-        'public'                => true,
-        'show_in_nav_menus'     => true, // равен аргументу public
-        'show_ui'               => true, // равен аргументу public
-        'show_tagcloud'         => true, // равен аргументу show_ui
-        'hierarchical'          => false,
-        'update_count_callback' => '',
-        'rewrite'               => true,
-        //'query_var'             => $taxonomy, // название параметра запроса
-        'capabilities'          => array(),
-        'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
-        'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
-        '_builtin'              => false,
-        'show_in_quick_edit'    => null, // по умолчанию значение show_ui
-    );
-    register_taxonomy('service_type', array('services'), $args );
 }
